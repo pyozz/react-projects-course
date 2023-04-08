c35107ff3b0b9c37c552e503d44158b4fcd57528
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-09 00:50:21 +0900

    🚧 Create global contexts

---
+++ 12-sidebar-modal/src/context/context.jsx
@@ -0,0 +1,48 @@
+import { createContext, useState } from 'react'
+
+export const AppContext = createContext({
+  isSidebarOpen: false,
+  isModalOpen: false,
+  openSidebar: () => {},
+  closeSidebar: () => {},
+  openModal: () => {},
+  closeModal: () => {},
+})
+
+function AppProvider({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export default AppProvider

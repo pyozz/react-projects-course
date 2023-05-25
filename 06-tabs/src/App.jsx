8776cd22c320c35dd2792ca53f81e8119c1f47e1
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-05-25 15:51:04 +0900

    🔥 Remove loading-related code

--- 06-tabs/src/App.jsx
+++ 06-tabs/src/App.jsx
@@ -7,11 +7,8 @@ const url = 'https://course-api.com/react-tabs-project'
 const App = () => {
   const [jobs, setJobs] = useState([])
   const [currentItem, setCurrentItem] = useState(0)
-  const [isLoading, setIsLoading] = useState(false)
 
   const fetchJobs = async () => {
-    setIsLoading(true)
-
     try {
       const response = await fetch(url)
       const responseData = await response.json()
@@ -19,21 +16,11 @@ const App = () => {
     } catch (error) {
       console.log(error)
     }
-
-    setIsLoading(false)
   }
 
   useEffect(() => {
     fetchJobs()
   }, [])
-
-  if (isLoading) {
-    return (
-      <section className="jobs-center">
-        <div className="loading"></div>
-      </section>
-    )
-  }
 
   return (
     <section className="jobs-center">
